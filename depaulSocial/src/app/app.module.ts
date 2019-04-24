ad0ab4d1fa80681a50b8fc20c34f97ab42729fd3
Heather Duke <hduke@mail.depaul.edu>
2019-04-23 19:54:59 -0500

    created angular files for join study

--- depaulSocial/src/app/app.module.ts
+++ depaulSocial/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JoinStudyComponent } from './join-study/join-study.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JoinStudyComponent
   ],
   imports: [
     BrowserModule,
