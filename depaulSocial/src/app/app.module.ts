716bd1fd8fbdf033b5f07c653fa2db1770c8299e
Heather Duke <hduke@mail.depaul.edu>
2019-04-23 19:24:10 -0500

    just made empty component

--- depaulSocial/src/app/app.module.ts
+++ depaulSocial/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeetupComponent } from './meetup/meetup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeetupComponent
   ],
   imports: [
     BrowserModule,
