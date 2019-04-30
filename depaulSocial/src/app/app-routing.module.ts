f9f2713f111e3e31665735a97a8ca280ee30ef78
jlama94 <lamajulio17@gmail.com>
2019-04-29 22:03:27 -0500

    First commit of meetUpPage.

--- depaulSocial/src/app/app-routing.module.ts
+++ depaulSocial/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LandingPageComponent} from "./landing-page/landing-page.component";
-import {StudyPageComponent} from "./study-page/study-page.component";
+import { LandingPageComponent} from "./landing-page/landing-page.component";
+import { StudyPageComponent} from "./study-page/study-page.component";
+import { MeetupComponent} from "./meetup/meetup.component";
 
 const routes: Routes = [
   {path: 'depaulSocial', component: LandingPageComponent},
   {path: '', redirectTo: '/depaulSocial', pathMatch: 'full'},
-  {path: 'study', component: StudyPageComponent}
+  {path: 'study', component: StudyPageComponent},
+  {path: 'meetup', component: MeetupComponent}
 ];
 
 @NgModule({
