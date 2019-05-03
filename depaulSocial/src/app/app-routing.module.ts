43c589a6ea84a5fc55d3bf3065ef990bc8cd26ad
t1nguy3n <tng.one@gmail.com>
2019-05-03 18:25:58 -0500

    removed embedded video and twitter out of buy page

--- depaulSocial/src/app/app-routing.module.ts
+++ depaulSocial/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LandingPageComponent} from "./landing-page/landing-page.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { TextbookBuyComponent } from "./textbook-buy/textbook-buy.component";
+import { TextbookSellComponent} from "./textbook-sell/textbook-sell.component";
 
 const routes: Routes = [
   {path: 'landingPage', component: LandingPageComponent},
-  {path: '', redirectTo: '/landingPage', pathMatch: 'full'}
+  {path: '', redirectTo: '/landingPage', pathMatch: 'full'},
+  { path: 'TextbookSellComponent', component: TextbookSellComponent },
+  { path: 'TextbookBuyComponent', component: TextbookBuyComponent }
 ];
 
 @NgModule({
