a2fbc66d294913542247e968cad92a0e322f8d2b
Neal Desai <ndesaics@gmail.com>
2019-05-08 23:49:13 -0500

    create meetup component created. still need to bind data

--- depaulSocial/src/app/app-routing.module.ts
+++ depaulSocial/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LandingPageComponent} from "./landing-page/landing-page.component";
+import { CreateMeetupComponent } from './create-meetup/create-meetup.component';
 
 const routes: Routes = [
   {path: 'landingPage', component: LandingPageComponent},
-  {path: '', redirectTo: '/landingPage', pathMatch: 'full'}
+  {path: '', redirectTo: '/landingPage', pathMatch: 'full'},
+  {path: 'createMeetup', component: CreateMeetupComponent},
 ];
 
 @NgModule({
