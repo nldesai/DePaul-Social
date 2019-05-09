94b41b14107f054336d6a36486d89a1925941d95
Linh Huynh <linhuy18@gmail.com>
2019-05-08 19:46:33 -0500

    Alex's components

--- depaulSocial/src/app/app-routing.module.ts
+++ depaulSocial/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import {LandingPageComponent} from "./landing-page/landing-page.component";
 import {StudyPageComponent} from "./study-page/study-page.component";
 import {HomepageComponent} from "./homepage/homepage.component";
 import {TextPageComponent} from "./text-page/text-page.component";
+import {CreateGroupComponent} from "./create-group/create-group.component";
+import {SearchPartnerComponent} from "./search-partner/search-partner.component";
 
 const routes: Routes = [
   {path: 'depaulSocial', component: LandingPageComponent},
   {path: '', redirectTo: '/depaulSocial', pathMatch: 'full'},
   {path: 'study', component: StudyPageComponent},
   {path: 'home', component: HomepageComponent},
-  {path: 'text', component: TextPageComponent}
+  {path: 'text', component: TextPageComponent},
+  {path: 'creategroup', component: CreateGroupComponent},
+  {path: 'searchpartner', component: SearchPartnerComponent}
 ];
 
 @NgModule({
--- depaulSocial/src/app/app.module.ts
+++ depaulSocial/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { StudyPageComponent } from './study-page/study-page.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { TextPageComponent } from './text-page/text-page.component';
+import { CreateGroupComponent } from './create-group/create-group.component';
+import { SearchPartnerComponent } from './search-partner/search-partner.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { TextPageComponent } from './text-page/text-page.component';
     LandingPageComponent,
     StudyPageComponent,
     HomepageComponent,
-    TextPageComponent
+    TextPageComponent,
+    CreateGroupComponent,
+    SearchPartnerComponent
   ],
   imports: [
     BrowserModule,
