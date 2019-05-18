5004c8f4289bb6c4bd55fb7b6668aa310a9f3029
coronary <cjdekalb@Gmail.com>
2019-05-17 19:40:07 -0500

    removed paragraph and buttons

--- depaulSocial/src/app/profile/profile.component.ts
+++ depaulSocial/src/app/profile/profile.component.ts
@@ -4,11 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
+  // , 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
 })
 export class ProfileComponent implements OnInit {
 	defaultPic: string;
 	classes: string[];
-  constructor() { 
+  constructor() {
 	this.defaultPic = "assets/t2.jpg";
 	this.classes = ['Class 1','Class 2','Class 3','Class 4']
 }
