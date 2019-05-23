8bf0556c9feba909843cc8ec04b4ae6ce3769947
Linh Huynh <linhuy18@gmail.com>
2019-05-23 18:45:19 -0500

    changes

--- depaulSocial/src/app/app-routing.module.ts
+++ depaulSocial/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { TextbookBuyComponent } from './textbook-buy/textbook-buy.component';
 import { TextbookSellComponent} from './textbook-sell/textbook-sell.component';
 import { AboutUsComponent} from './about-us/about-us.component';
 import { ProfileComponent} from './profile/profile.component';
+import {MeetupComponent} from './meetup/meetup.component';
 import { CreateMeetupComponent } from './create-meetup/create-meetup.component';
 import { FindMeetupComponent } from './find-meetup/find-meetup.component';
 import { StudyPageComponent} from './study-page/study-page.component';
@@ -20,19 +21,12 @@ import {TextbookSwapComponent} from "./textbook-swap/textbook-swap.component";
 const routes: Routes = [
   {path: 'landingPage', component: LandingPageComponent},
   {path: '', redirectTo: '/landingPage', pathMatch: 'full'},
-  {path: 'booksell', component: TextbookSellComponent },
-  {path: 'bookbuy', component: TextbookBuyComponent },
   {path: 'aboutUs', component: AboutUsComponent},
   {path: 'profile', component: ProfileComponent},
-  {path: 'createMeetup', component: CreateMeetupComponent},
-  {path: 'findMeetup', component: FindMeetupComponent},
-  {path: 'depaulSocial', component: LandingPageComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomepageComponent},
-  {path: 'text', component: TextPageComponent},
-  {path: 'bookswap', component: TextbookSwapComponent},
-
+  
   {
     path: 'study',
     component: StudyPageComponent,
@@ -41,6 +35,25 @@ const routes: Routes = [
       {path: 'join-study-group', component: JoinStudyComponent},
       {path: 'create-group', component: CreateGroupComponent},
       {path: 'searchpartner', component: SearchPartnerComponent}
+    ]
+  },
+
+  {
+    path: 'text',
+    component: TextPageComponent,
+    children: [
+      {path: 'booksell', component: TextbookSellComponent},
+      {path: 'bookbuy', component: TextbookBuyComponent},
+      {path: 'bookswap', component: TextbookSwapComponent}
+    ]
+  },
+
+  {
+    path: 'meetup',
+    component: MeetupComponent,
+    children: [
+      {path: 'createMeetup', component: CreateMeetupComponent},
+      {path: 'findMeetup', component: FindMeetupComponent}
     ]
   }
   ];
