8c29524eac4e372fb88bd4f19a023033b8912d99
Julio <lamajulio17@gmail.com>
2019-05-28 10:36:20 -0500

    Removed comment & added environment.prod.ts info

--- depaulSocial/src/environments/environment.prod.ts
+++ depaulSocial/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAbEUyzdzNxfK7dGfnbRwhU4A_ZmPzSneM',
+    authDomain: 'depaulsocial-c76a3.firebaseapp.com',
+    databaseURL: 'https://depaulsocial-c76a3.firebaseio.com',
+    projectId: 'depaulsocial-c76a3',
+    storageBucket: 'depaulsocial-c76a3.appspot.com',
+    messagingSenderId: '92498946152',
+    appId: '1:92498946152:web:2891e9c9bda68dbe'
+  }
 };
